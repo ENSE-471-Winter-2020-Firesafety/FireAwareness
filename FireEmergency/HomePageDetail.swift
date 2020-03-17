1e3b613a1b9c84727e0b2d951d68cbc745c1e794
Nick-liu666 <55857483+Nick-liu666@users.noreply.github.com>
2020-03-17 16:28:13 -0600

    buttons

--- FireEmergency/HomePageDetail.swift
+++ FireEmergency/HomePageDetail.swift
@@ -15,9 +15,20 @@ struct HomePageDetail: View {
        // NavigationView
        // {
         ZStack {
+//            .forcegroundColor()
+//            Color(red: 0.5, green: 0.3, bule: 0.4)
+//            Color.pink.edgesIgnoringSafeArea(.bottom)
             
-            Color.pink.edgesIgnoringSafeArea(.bottom)
             
+            Rectangle()
+                .foregroundColor(Color(red: 195/255, green: 140/255, blue: 148/255))
+                .edgesIgnoringSafeArea(.bottom)
+            
+            Rectangle()
+            .foregroundColor(Color(red: 203/255, green: 108/255, blue: 99/255))
+                .frame(height: 80)
+                .cornerRadius(16.0)
+                .position(x: 207, y: 40)
             
             VStack {
                 HStack {
@@ -31,7 +42,11 @@ struct HomePageDetail: View {
                 }
                 .background(LinearGradient(gradient: Gradient(colors: [Color.pink , Color.purple]), startPoint: .leading, endPoint: .trailing))
                 Spacer()
-
+                HStack{
+                    Image("phone").resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.black.opacity(0.5))
+                    Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Practice Phone Call")
                         .font(.title)
@@ -41,10 +56,53 @@ struct HomePageDetail: View {
                         .padding(.leading)
                         .frame(width: 300.0, height: 100.0)
                         .background(/*@START_MENU_TOKEN@*/Color.orange/*@END_MENU_TOKEN@*/)
-                        .position(x: 200, y: 150)
+                        .cornerRadius(20.0)
+                        .shadow(radius: /*@START_MENU_TOKEN@*/6/*@END_MENU_TOKEN@*/)
                         
-                }
 
+                }
+                }
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                   Text("Draw Escape Plan")
+                                       .font(.title)
+                                       .fontWeight(.medium)
+                                       .foregroundColor(Color.black)
+                                       .multilineTextAlignment(.center)
+                                       .padding(.leading)
+                                       .frame(width: 300.0, height: 100.0)
+                                    .background(Color(red: 255/255, green: 172/255, blue: 4/255))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                               }
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                   Text("Fire Survival Info")
+                                       .font(.title)
+                                       .fontWeight(.medium)
+                                       .foregroundColor(Color.black)
+                                       .multilineTextAlignment(.center)
+                                       .padding(.leading)
+                                       .frame(width: 300.0, height: 100.0)
+                                    .background(Color(red: 251/255, green: 219/255, blue: 123/255))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+//                                       .position(x: 200, y: 150)
+                               }
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                   Text("Fire Safety Info")
+                                       .font(.title)
+                                       .fontWeight(.medium)
+                                       .foregroundColor(Color.black)
+                                       .multilineTextAlignment(.center)
+                                       .padding(.leading)
+                                       .frame(width: 300.0, height: 100.0)
+                                    .background(Color(red: 251/255, green: 251/255, blue: 4/255))
+                                      .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                               }
+                Spacer()
             }.padding()
                 
         }
