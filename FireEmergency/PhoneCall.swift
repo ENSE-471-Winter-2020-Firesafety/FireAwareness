1d8340d6013bf190c3700cb638816c5b8cd91163
moehared <46978582+moehared@users.noreply.github.com>
2020-03-20 01:40:23 -0600

    Updated Phone Screen View

--- FireEmergency/PhoneCall.swift
+++ FireEmergency/PhoneCall.swift
@@ -14,10 +14,7 @@ struct PhoneCall: View
     @Environment(\.presentationMode) var presentationMode
     @State var isClicked = false
     @State var helpMode = false
-     @State var backButton = false
     var showClearIcon = false
-    
-
     
     let buttons = [
         ["1" , "2" , "3"],
@@ -28,141 +25,124 @@ struct PhoneCall: View
     
     var body: some View
     {
-        ZStack(alignment: .bottom) {
-           
+        
+        ZStack {
+           Color.white.edgesIgnoringSafeArea(.all)
             
-            Color.white.edgesIgnoringSafeArea(.all)
-            VStack (spacing: 20) {
-               
-                HStack {
-                        Button(action: {
-                            //self.presentationMode.wrappedValue.dismiss()
-                            self.backButton.toggle()
-                         }, label: {
-                                
-                   //       Image("leftArrow").foregroundColor(.black)
-                            Image(systemName: "arrow.left.circle")
-                                .resizable()
-                                .frame(width: 40, height: 40, alignment: .leading)
-                                .foregroundColor(.black)
-                            Spacer()
-                            
-                    })
-           
-                    Button(action: {
-                       
-                        self.isClicked.toggle()
-                    
-                    }, label: {
-                        VStack {
-                            HStack {
-                                Image("setting")
-                                 .foregroundColor(.white)
-                                .background(Color.black)
-                            }
-                            
-                            Text("Option")
-                                .foregroundColor(.black)
-                                .fontWeight(.semibold)
-                            
-                        }
-                    })
-                }
-                .padding()
-                
-                 Spacer()
+            VStack {
                 
                 VStack {
                     HStack {
-                        if isClicked {
-                            Toggle(isOn: $helpMode, label: {
-                                HStack {
-                                    Text("Help Mode")
-                                        .font(.system(size: 30))
-                                        .foregroundColor(.black)
-                                }
-                                Spacer()
-                            })
-                        }
+                        Button(action: {
+                            
+                        }, label: {
+                              Image(systemName: "arrow.left.circle")
+                               .resizable()
+                               .frame(width: 40, height: 40, alignment: .leading)
+                                .foregroundColor(.black)
+                             //   .background(Color.black)
+                        })
                         
-                    }
-                    Spacer()
-                }
-                Spacer()
- 
-                Text(env.display).foregroundColor(.black)
-                    .font(.system(size:64))
-                
-                     ForEach(buttons , id: \.self) { row in
-                        HStack(spacing: 20) {
-                             
-                             ForEach(row , id:\.self) { button in
-                
-                                 Button(action: {
-                                    
-                                    self.env.receieveInput(input: button)
-                                    
-                                 }, label: {
-                                     Text(button)
-                                 })
-                             }
-                             .buttonStyle(CircleStyle())
-
-                         }
+                        Spacer()
                         
-                     }
+                        Button(action: {
+                            self.isClicked.toggle()
+                        }, label: {
+                               Image("setting")
+                                .foregroundColor(.white)
+                                 .background(Color.black)
+                        })
+                        
+                    } // end of HStack option and backButton
                 
                     HStack {
-                        HStack {
-                            Button(action: {}) {
-                            Image(systemName: "phone")
-                                
-                        }
-                            .padding(.top)
-                         .buttonStyle(CallPhoneStyle())
-                              //Spacer()
-                        }
-                        .padding(.trailing,10)
-                        .frame(width: 105, height: 20, alignment: .leading)
-                        
-                        Text(" ")
-                        HStack {
-                        Button(action: {
-                                    
-                               self.env.isPresented.toggle()
-                                    
-                                    self.env.display.removeLast()
-                                    
-                                }, label: {
-                                    
-                                   if !self.env.display.isEmpty {
-                                        Image(systemName: "xmark.circle.fill")
-                                        .foregroundColor(.black)
-                                            .font(.system(size:20))
-                                          
-                                    }
+                        if isClicked {
+                            Toggle(isOn: $helpMode, label: {
+                                Text("Help Mode")
+                                .font(.system(size: 30))
+                                .foregroundColor(.black)
+
                             })
-                        }
-                        .padding(.leading,10)
-
-                    }
-                        .frame(width: 200, height: 20, alignment: .trailing)
+                            
+                        } // end of if
                         
-                    .padding(10)
-               
-                 } // end of Vstack
-                .padding(.bottom,30)
+                    } // end of HStack helpMode
+                    
+                } // end of VStack for helpMode , back button and option
+                    
+                .padding()
+                
+                Spacer()
+
+                VStack {
+                    Text(env.display).foregroundColor(.black)
+                    .font(.system(size:30))
+     
+                     ForEach(buttons , id: \.self) { row in
+                        HStack {
+                                ForEach(row , id:\.self) { button in
+                                    Button(action: {
+                                            self.env.receieveInput(input: button)
+                                        
+                                            }, label: {
+                                                
+                                          Text(button)
+                                     })
+                                    
+                            } // end of forEach in each row
+                            
+                            .buttonStyle(CircleStyle())
+                    
+
+                        } // end of HStack for buttons
+                       
+                    } //  end of forEach in row
+    
+                    
+                } // end of VStack for all of the the buttons
+                   .padding(.bottom,20)
+                
+                HStack {
+                    Button(action: {}) {
+                        Image(systemName: "phone")
+                    }
+                     .buttonStyle(CallPhoneStyle())
+                    .padding(.top,10)
+                   
+                    
+                    Button(action: {
+                        self.env.isPresented.toggle()
+                        
+                        self.env.display.removeLast()
+                        
+                    }, label: {
+                        if !self.env.display.isEmpty {
+                            Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.black)
+                            .font(.system(size:20))
+                            .frame(width: 40, height: 10, alignment: .trailing)
+                        } // end of if
+                    })
+                    
+
+                } // end of HStack for Call button and clear
+                      .padding(.bottom,20)
+                .padding(.leading,40)
         
-        }
-
-}
-
-    struct PhoneCall_Previews: PreviewProvider
-    {
-        static var previews: some View
-        {
-            PhoneCall().environmentObject(UserData())
-        }
+            } // end of VStack for all of the the view
+   
+        } // end ZStack
+            
     }
-        
+
 }
+
+struct PhoneCall_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        PhoneCall().environmentObject(UserData())
+    }
+}
+        
+
